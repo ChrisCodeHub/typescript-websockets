0203aca4b94bc9280ad95ba46c6258b3a10f666b
chris <chr.duxbury@gmail.com>
2025-10-06 17:49:25 +0100

    add more routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import http from 'http'
-import { WebSocketServer } from 'ws'
+import WebSocket, { WebSocketServer } from 'ws'
+
 
 
 const app = express();
@@ -9,9 +10,25 @@ app.use(express.static('public'))
 
 const wss = new WebSocketServer({server});
 
-wss.on("connection", (ws)=> {
+// send the request as well so that we can pick up the specific patjs
+wss.on("connection", (ws, req)=> {
     console.log(` websocket connected `);
-    
+
+
+    const path = req.url; // Get the path from the request
+
+    if (path === '/chat'){
+        openChatWebsocket(ws);
+    } else if (path === 'notifications'){
+        openNotificationsWebsocket(ws);
+    }else if (path?.startsWith('/game/')) {
+        const roomId = path.split('/')[2]; // Extract 'room123'
+        handleGame(ws, roomId);
+    } else {
+        console.log(` Did not recognise the URL ${path}, closing socket `)
+        ws.close;
+    }
+
     // Send a welcome message
     ws.send('Welcome to the WebSocket server!');
 
@@ -29,6 +46,39 @@ wss.on("connection", (ws)=> {
     });
 
 });
+
+function openChatWebsocket(ws:WebSocket){
+    console.log(" opened the openChatWebsocket ");
+    
+    // Send a welcome message
+    ws.send('Welcome to the WebSocket chatroom');
+
+    // messages get data, in this case we will just echo back and add a note
+    ws.on("message", (data) => {
+        ws.send(`Server received: ${data}`);
+    });
+    
+    ws.on("close", () => {
+        console.log(` closed the websocket`);
+    });
+}
+
+
+function openNotificationsWebsocket(ws:WebSocket){
+
+    const notifloop = setInterval( () => {        
+        ws.send(`Notification at ${new Date().toLocaleTimeString()}`);
+    }, 5000);
+
+    ws.on("close", ()=>{
+        clearInterval(notifloop);
+        console.log("closed notifications socket");
+    })
+}
+
+function handleGame(ws : WebSocket, roomId : string){
+
+}
 
 
 // Start server
